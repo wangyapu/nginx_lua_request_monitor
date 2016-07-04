5385f18a788cbad65078a03e47b77e51f6bfc955
wangyapu <570754729@qq.com>
2016-07-04 16:59:31 +0800

    project structure

---
+++ reqstat.lua
@@ -0,0 +1,10 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: wangyapu
+-- Date: 16/7/4
+-- Time: 下午4:54
+-- To change this template use File | Settings | File Templates.
+--
+
+
+
---
+++ status_api.lua
@@ -0,0 +1,8 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: wangyapu
+-- Date: 16/7/4
+-- Time: 下午4:58
+-- To change this template use File | Settings | File Templates.
+--
+
