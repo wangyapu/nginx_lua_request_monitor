2f48f9c3d5b5a195cc5a1d380170bcc5e4f8f50d
wangyapu <570754729@qq.com>
2016-07-04 17:22:32 +0800

    dict incr function

--- status_api.lua
+++ status_api.lua
@@ -6,3 +6,14 @@
 -- To change this template use File | Settings | File Templates.
 --
 
+local request_monitor = {}
+
+local function incr(dict, key, increment)
+    increment = increment or 1
+    local new_value, err = dict.incr()
+    if err then
+        dict:set(key, increment)
+        new_value = increment
+    end
+    return new_value
+end
